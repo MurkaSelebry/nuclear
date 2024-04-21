3934b40796a5c5cb6d1009773f7a23b934cc7c68
Алексей Смирнов <alekseysmirnov2003@gmail.com>
2024-04-21 13:21:03 +0300

    final

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ from flask import Flask, request, send_file, render_template, redirect
 import os
 app = Flask(__name__)
 script_path = "main.py"
+path1 = ''
+path2 = ''
 path3 = os.path.join('uploads', "green.geojson")
-
+check_upload = False
 @app.route('/', methods=['GET', 'POST'])
 def upload_files():
     if request.method == 'POST':
@@ -15,13 +17,55 @@ def upload_files():
         path2 = os.path.join('uploads', file2.filename)
         file1.save(path1)
         file2.save(path2)
+        file1.filename = 'blue.geojson'
+        file2.filename = 'red.geojson'
+        os.system(f"py gen_pic.py uploads\\{file1.filename} uploads\\{file2.filename} uploads\\green.geojson")
         exec(open(script_path).read(), {'blue_path': os.path.abspath(path1),
                                       'red_path': os.path.abspath(path2),
                                       'green_path': os.path.abspath(path3)})
-        send_file(path3, as_attachment=True)
-        return redirect('/images')
-    return render_template('index.html')
+        return send_file(path3, as_attachment=True)
 
+    return render_template('index.html')
+@app.route('/images', methods=['GET', 'POST'])
+def show_images():
+    #exec(open("gen_pic.py").read(), {'blue_path': os.path.abspath(os.path.join('uploads', "blue.geojson")),
+    #                                'red_path': os.path.abspath(os.path.join('uploads', "red.geojson")),
+    #                                'green_path': os.path.abspath(os.path.join('uploads', "green.geojson"))})
+    return '''
+    <!DOCTYPE html>
+<html>
+<head>
+    <title>Изображения</title>
+    <style>
+        .image-container {
+            text-align: center;
+            margin: 20px auto;
+        }
+        .image-container img {
+            width: 200px;
+            height: auto;
+        }
+        .image-container .caption {
+            font-size: 14px;
+            margin-top: 10px;
+        }
+    </style>
+</head>
+<body>
+    <div class="image-container">
+        <img src="/static/red_photo.png" alt="Красный граф">
+        <div class="caption">Красный граф: Изображение</div>
+    </div>
+    <div class="image-container">
+        <img src="/static/blue_photo.png" alt="Синий граф">
+        <div class="caption">Синий граф: Изображение</div>
+    </div>
+    <div class="image-container">
+        <img src="/static/green_photo.png" alt="Зеленый граф">
+        <div class="caption">Зеленый граф: Изображение</div>
+    </div>
+</body>
+    '''
 
 
 if __name__ == '__main__':
--- gen_pic.py
+++ gen_pic.py
@@ -1,19 +1,23 @@
 import matplotlib.pyplot as plt
 import geopandas as gpd
+from sys import argv
 
-
-blue_path = globals().get('blue_path', 'C:\\Users\\aleks\PycharmProjects\pythonProject\j\\blue.geojson')
-red_path = globals().get('red_path', 'C:\\Users\\aleks\PycharmProjects\pythonProject\j\\red.geojson')
-green_path = globals().get('green_path', 'C:\\Users\\aleks\PycharmProjects\pythonProject\j\\green.geojson')
-
+blue_path = argv[1]
+red_path = argv[2]
+green_path = argv[3]
+#blue_path = globals().get('blue_path', 'C:\\Users\\aleks\PycharmProjects\pythonProject\j\\blue.geojson')
+#red_path = globals().get('red_path', 'C:\\Users\\aleks\PycharmProjects\pythonProject\j\\red.geojson')
+#green_path = globals().get('green_path', 'C:\\Users\\aleks\PycharmProjects\pythonProject\j\\green.geojson')
+print(blue_path)
 df_blue = gpd.read_file(blue_path)
 df_blue.plot(color='b')
-plt.savefig('')
+plt.savefig('static/blue_photo.png')
+
 
 df_red = gpd.read_file(red_path)
 df_red.plot(color='r')
-plt.savefig('')
+plt.savefig('static/red_photo.png')
 
 df_green = gpd.read_file(green_path)
 df_green.plot(color='g')
-plt.savefig('')
+plt.savefig('static/green_photo.png')
